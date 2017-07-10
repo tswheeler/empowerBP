1ae5c74ef485a4884cb3d0c171bed7ffca416c81
Tyler Wheeler <tylerswheeler@192.168.2.11>
2017-07-09 21:13:27 -0300

    Initial Commit

---
+++ EmpowerBP/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  EmpowerBP
+//
+//  Created by Tyler S Wheeler on 2017-07-09.
+//  Copyright © 2017 Tyler S Wheeler. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
